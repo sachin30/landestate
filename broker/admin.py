1de53582508b895258f6d34099bb24f10af23b7f
sachin30 <sachinpandhare1996@gmail.com>
2022-09-03 18:32:26 +0530

    Update admin.py

--- broker/admin.py
+++ broker/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from broker.models import Broker
 from django.utils.html import format_html
 
-# Register your models here.
+# Register your models here
 class BrokerAdmin(admin.ModelAdmin):
     def broker_photo(self,obj):
         return format_html(f"<img src='/media/{obj.photo}' style='height:100px ;width:100px' >")
